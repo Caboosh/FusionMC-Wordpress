1f4e579a16c059337f5660e9d9587f73c623edb4
Cameron Miller <cambomiller@googlemail.com>
2019-09-23 13:19:00 +0100

    Update Blog Template

--- home.php
+++ home.php
@@ -23,110 +23,53 @@
     <div class="container">
         <div class="columns is-multiline">
             <div class="column is-8">
-                <!-- start of post -->
-                <div class="box">
-                    <!-- box header -->
-                    <div class="box-header">
-                        <h4 class="title is-4">
-                            Python Has Some Problems I Like
-                            <span class="labels is-pulled-right">
-		    <a href="label.html">Label 3</a>
-		  </span>
-                        </h4>
-                        <small>Posted by <a>Author</a> at 10:00am, February 25th 2010</small>
-                    </div>
-                    <!-- end of box header -->
-                    <!-- box content -->
-                    <div class="box-content">
-                        <div class="columns">
-                            <div class="column is-4">
-                                <img src="http://placehold.it/250x250">
-                            </div>
-                            <div class="column">
-                                <div class="box-content-text">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nulla tortor, congue a venenatis at, ornare eu dui. Mauris vehicula malesuada nisl, at tristique nibh pretium vitae. Donec mattis, neque et auctor condimentum, libero mauris molestie ex, sit amet consectetur arcu sapien vitae justo. Fusce urna nulla, aliquam id enim nec, egestas tincidunt orci. Proin porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nulla tortor, congue a venenatis at, ornare eu dui. Mauris vehicula malesuada nisl, at tristique nibh pretium vitae.</p>
+                <?php
+                    if ( have_posts() )
+                    {
+                        while ( have_posts() ) {
+                            the_post();
+                            ?>
+                            <!-- start of post -->
+                            <div class="box">
+                                <!-- box header -->
+                                <div class="box-header">
+                                    <h4 class="title is-4">
+                                        <?= get_the_title(); ?>
+                                    </h4>
+                                    <small>Posted by <a><?= get_the_author(); ?></a> at <?= get_the_time(); ?>, <?= get_the_date(); ?></small>
                                 </div>
-                                <div class="has-text-right">
-                                    <a class="button is-small" href="post.html">Read More</a>
+                                <!-- end of box header -->
+                                <!-- box content -->
+                                <div class="box-content">
+                                    <div class="columns">
+                                        <div class="column is-4 is-250px">
+                                            <?php the_post_thumbnail(); ?>
+                                        </div>
+                                        <div class="column">
+                                            <div class="box-content-text">
+                                                <p>
+                                                    <?= get_the_excerpt(); ?>
+                                                </p>
+                                            </div>
+                                            <div class="has-text-right">
+                                                <a class="button is-small" href="<?= get_the_permalink(); ?>">Read More</a>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
+                                <!-- end box content -->
                             </div>
-                        </div>
-                    </div>
-                    <!-- end box content -->
-                </div>
-                <!-- end of post -->
-
-                <!-- start of post -->
-                <div class="box">
-                    <!-- box header -->
-                    <div class="box-header">
-                        <h4 class="title is-4">
-                            Python Has Some Problems I Like
-                            <span class="labels is-pulled-right">
-		    <a href="label.html">Label 3</a>
-		  </span>
-                        </h4>
-                        <small>Posted by <a>Author</a> at 10:00am, February 25th 2010</small>
-                    </div>
-                    <!-- end of box header -->
-                    <!-- box content -->
-                    <div class="box-content">
-                        <div class="columns">
-                            <div class="column is-4">
-                                <img src="http://placehold.it/250x250">
-                            </div>
-                            <div class="column">
-                                <div class="box-content-text">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nulla tortor, congue a venenatis at, ornare eu dui. Mauris vehicula malesuada nisl, at tristique nibh pretium vitae. Donec mattis, neque et auctor condimentum, libero mauris molestie ex, sit amet consectetur arcu sapien vitae justo. Fusce urna nulla, aliquam id enim nec, egestas tincidunt orci. Proin porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nulla tortor, congue a venenatis at, ornare eu dui. Mauris vehicula malesuada nisl, at tristique nibh pretium vitae.</p>
-                                </div>
-                                <div class="has-text-right">
-                                    <a class="button is-small" href="post.html">Read More</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- end box content -->
-                </div>
-                <!-- end of post -->
-
-                <!-- start of post -->
-                <div class="box">
-                    <!-- box header -->
-                    <div class="box-header">
-                        <h4 class="title is-4">
-                            Python Has Some Problems I Like
-                            <span class="labels is-pulled-right">
-		    <a href="label.html">Label 3</a>
-		  </span>
-                        </h4>
-                        <small>Posted by <a>Author</a> at 10:00am, February 25th 2010</small>
-                    </div>
-                    <!-- end of box header -->
-                    <!-- box content -->
-                    <div class="box-content">
-                        <div class="columns">
-                            <div class="column is-4">
-                                <img src="http://placehold.it/250x250">
-                            </div>
-                            <div class="column">
-                                <div class="box-content-text">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nulla tortor, congue a venenatis at, ornare eu dui. Mauris vehicula malesuada nisl, at tristique nibh pretium vitae. Donec mattis, neque et auctor condimentum, libero mauris molestie ex, sit amet consectetur arcu sapien vitae justo. Fusce urna nulla, aliquam id enim nec, egestas tincidunt orci. Proin porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nulla tortor, congue a venenatis at, ornare eu dui. Mauris vehicula malesuada nisl, at tristique nibh pretium vitae.</p>
-                                </div>
-                                <div class="has-text-right">
-                                    <a class="button is-small" href="post.html">Read More</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- end box content -->
-                </div>
-                <!-- end of post -->
+                            <!-- end of post -->
+                        <?php
+                        }
+                    }
+                ?>
 
                 <!-- pagination -->
                 <nav class="pagination">
                     <div class="pagination-fields">
-                        <a class="button" disabled>Previous</a>
-                        <a class="button">Next page</a>
+                        <a class="button" href="<?php previous_post_link('&larr; Previous Page'); ?>">Previous Page</a>
+                        <a class="button" href="<?php next_post_link('&rarr; Next page'); ?>">Next page</a>
                     </div>
                 </nav>
                 <!-- end of pagination -->
@@ -135,58 +78,9 @@
             <div class="column is-4 is-narrow">
                 <!-- sidebar subscriptions -->
                 <div class="card-wrapper">
-                    <div class="card">
-                        <header class="card-header ">
-                            <p class="card-header-title">
-                                Subscribe
-                            </p>
-                        </header>
-                        <div class="card-content">
-                            <div class="content">
-                                <p>Some text about subscribing. Whatever you might want. You know.</p>
-                                <form>
-                                    <div class="field">
-                                        <p class="control">
-                                            <input class="input" type="email" placeholder="Email">
-                                        </p>
-                                    </div>
-                                    <div class="field is-grouped">
-                                        <p class="control">
-                                            <button class="button">Subscribe</button>
-                                        </p>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
+                    <?php get_sidebar('cf_sidebar-default') ?>
                 </div>
                 <!-- end of sidebar subscriptions -->
-
-                <!-- sidebar labels -->
-                <div class="card-wrapper">
-                    <div class="card">
-                        <header class="card-header">
-                            <p class="card-header-title">
-                                Labels
-                            </p>
-                        </header>
-                        <div class="card-content">
-                            <div class="content">
-                                <div class="labels">
-                                    <a href="label.html">Label 1</a>
-                                    <a href="label.html">Label 2</a>
-                                    <a href="label.html">Label 3</a>
-                                    <a href="label.html">Label 4</a>
-                                    <a href="label.html">Label 5</a>
-                                    <a href="label.html">Label 6</a>
-                                    <a href="label.html">Label 7</a>
-                                    <a href="label.html">Label 8</a>
-                                    <a href="label.html">Label 9</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- end of sidebar labels -->
                 </div>
                 <!-- end of sidebar content -->
             </div>
